543a892dc414851e4341b6c267a526bc26369ba2
senornikos <senornikos@gmail.com>
2013-07-13 09:57:50 +0000

    Updated buffer again in web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(fs.readFileSync('index.html', "utf-8"));
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8',0,27));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
